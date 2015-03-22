14bf9140171a3fedd42d93420c3837bd2c70d43b
Sachin Shekhar R <sachin.r.r@gmail.com>
2015-03-22 22:56:27 +0530

    Task - Query Execution for multiple groupby.
    1. Fixed issue with hsqldb not cleaning up data before each run.

--- src/main/java/com/appbootup/hibernate/CaseInstanceApp.java
+++ src/main/java/com/appbootup/hibernate/CaseInstanceApp.java
@@ -1,29 +1,26 @@
 package com.appbootup.hibernate;
 
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Projections;
 
-/**
- * Hello world!
- * 
- */
 public class CaseInstanceApp {
+	static SessionFactory sessionFactory = HibernateUtil.configureSessionFactory();
+	static Session session = sessionFactory.getCurrentSession();
+
 	@SuppressWarnings({ "unchecked", "deprecation" })
 	public static void main(String[] args) {
-		SessionFactory sessionFactory = HibernateUtil.configureSessionFactory();
-		Session session = sessionFactory.getCurrentSession();
 
 		// We must open a new transaction before doing anything with the DB
 		Transaction tx = session.beginTransaction();
+		hqlTruncate("CaseInstance");
 
 		session.persist(new CaseInstance("open", "high", new Date(2015, 1, 1),
 				2015, 10));
@@ -52,38 +49,47 @@ public class CaseInstanceApp {
 				Projections.projectionList()
 						.add(Projections.groupProperty("cseSeverity"))
 						.add(Projections.groupProperty("cseCreatedYear"))
-						.add(Projections.max("cseCost"))
-						.add(Projections.min("cseCost"))
+						.add(Projections.count("cseCost"))
 						.add(Projections.sum("cseCost"))).list();
 		List results = criteria.list();
 		Object[] array = results.toArray();
 		for (Object object : array) {
 			Object[] data = (Object[]) object;
-			System.out.println("Case with cseSeverity " + data[0]
-					+ " have max cost of " + data[2] + " and min cost of "
-					+ data[3] + " and total cost of " + data[4] + " in year "
-					+ data[1]);
+			System.out.print("cseCreatedYear: " + data[1]);
+			System.out.print(" , cseSeverity: " +  data[0]);
+			System.out.println(" , sum of cseCost: " + data[3] );
 		}
 
-		String sql = "SELECT * FROM CaseInstance WHERE cseId = :cse_id";
-		SQLQuery query = session.createSQLQuery(sql);
-		query.addEntity(CaseInstance.class);
-		query.setParameter("cse_id", 2);
-		List sqlResults = query.list();
-		for (Iterator iterator = sqlResults.iterator(); iterator.hasNext();) {
-			CaseInstance caseInstance = (CaseInstance) iterator.next();
-			System.out.print("cse_id: " + caseInstance.getCseId()
-					+ " - cse_severity: " + caseInstance.getCseSeverity());
-		}
+		// String sql = "SELECT * FROM CaseInstance WHERE cseId = :cse_id";
+		// SQLQuery query = session.createSQLQuery(sql);
+		// query.addEntity(CaseInstance.class);
+		// query.setParameter("cse_id", 2);
+		// List sqlResults = query.list();
+		// for (Iterator iterator = sqlResults.iterator(); iterator.hasNext();) {
+		// CaseInstance caseInstance = (CaseInstance) iterator.next();
+		// System.out.print("cse_id: " + caseInstance.getCseId()
+		// + " - cse_severity: " + caseInstance.getCseSeverity());
+		// }
 
-		String sqlFinal = "SELECT cseCreatedYear, cseSeverity, count(cseCost) FROM CaseInstance GROUP BY cseSeverity, cseCreatedYear ";
+		String sqlFinal = "SELECT sum(cseCost), count(cseCost), cseCreatedYear, cseSeverity FROM CaseInstance GROUP BY cseCreatedYear, cseSeverity ";
 		SQLQuery queryFinal = session.createSQLQuery(sqlFinal);
 		List<Object[]> sqlFinalResults = queryFinal.list();
 		for (Object object : sqlFinalResults) {
 			Object[] data = (Object[]) object;
-			System.out.print("cseCreatedYear: " + data[0]);
-			System.out.print(" , cseSeverity: " + data[1]);
-			System.out.println(" , count of cseCost: " + data[2]);
+			System.out.print("cseCreatedYear: " + data[2]);
+			System.out.print(" , cseSeverity: " + data[3]);
+			System.out.println(" , sum of cseCost: " + data[0]);
+		}
+
+		String sqlFinalAgain = "SELECT sum(cseCost), count(cseCost), cseCreatedYear, cseSeverity, count(*) FROM CaseInstance GROUP BY cseSeverity, cseCreatedYear ORDER BY cseCreatedYear ";
+		SQLQuery queryFinalAgain = session.createSQLQuery(sqlFinalAgain);
+		List<Object[]> sqlFinalResultsAgain = queryFinalAgain.list();
+		for (Object object : sqlFinalResultsAgain) {
+			Object[] data = (Object[]) object;
+			System.out.print("cseCreatedYear: " + data[2]);
+			System.out.print(" , cseSeverity: " + data[3]);
+			System.out.print(" , sum of cseCost: " + data[0]);
+			System.out.println(" , count of cseCost: " + data[1]);
 		}
 
 		// List<CaseInstance> caseInstances = createCriteria.list();
@@ -95,5 +101,11 @@ public class CaseInstanceApp {
 		// }
 		// We can now close the transaction and persist the changes
 		tx.commit();
+	}
+	
+	public static int hqlTruncate(String truncateTable){
+	    String hql = String.format("delete from %s",truncateTable);
+	    Query query = session.createQuery(hql);
+	    return query.executeUpdate();
 	}
 }
